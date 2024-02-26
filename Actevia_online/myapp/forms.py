a9b3e1af2ab1c57d99ba8d0a8fbc7dfaec131307
0188cs181047 <shivprataprathour20@gmail.com>
2024-02-26 20:23:28 +0530

    add text file

--- Actevia_online/myapp/forms.py
+++ Actevia_online/myapp/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from .models import Policy,Upload
+from multiupload.fields import MultiFileField
+
 
 class PolicyForm(forms.ModelForm):
-    upload_files = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    upload_files = MultiFileField(min_num=1, max_num=20, max_file_size=1024*1024*5)
     policy_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter policy name'}))
 
     class Meta:
